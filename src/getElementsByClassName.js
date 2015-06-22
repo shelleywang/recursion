2802bcb3d94926cc670de35dde383d6fde7baecd
Shelley Wang <shelleyw13@gmail.com>
2015-06-22 00:53:37 -0700

    Completed StringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var results = []; // array to store the elements
+
+  function helper(doc, className) { // gets elements recursively
+    if (doc.nodeType === 1) { // excludes text and comment nodes    
+      if (doc.classList.contains(className)) {
+        results.push(doc);
+      }
+      if (doc.hasChildNodes()) {
+        var children = doc.childNodes;
+        for (var i = 0; i < children.length; i++) {
+          helper(children[i],className);
+        }
+    }
+  }
+  }
+
+  helper(document.body,className); // call recursive helper function on document.body
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var type = typeof obj;
+  var contains = [];
+  if (obj === null) {
+    return 'null';
+  } else if ((type === 'boolean') || (type ==='number')) {
+    return obj.toString();
+  } else if (type === 'string') {
+    return '"'+obj+'"';
+  } else if ((type === 'function') || (type ==='undefined')){
+    return null;
+  } else if (Array.isArray(obj)) {
+    contains = obj.map(function(item) {
+      return stringifyJSON(item);
+    });
+    return '['+contains.join(',')+']';
+  } else if (type === 'object') {
+    each(obj, function(value, item,obj) {
+      if (stringifyJSON(value) !== null) {
+        contains.push('"'+item+'":'+stringifyJSON(value));
+      }
+    });
+    return '{' + contains.join(',')+'}';
+  }
 };
+
+
+// helper function 
+var each = function(collection, iterator) {
+      if (Array.isArray(collection)) {
+        for (var i = 0; i < collection.length; i++) {
+          iterator(collection[i], i, collection);
+        }
+      } else {
+        for (var item in collection) {
+          iterator(collection[item], item, collection);
+        }
+      }
+    
+  };
